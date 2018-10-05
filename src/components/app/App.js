203deb096b950fc8b40fcecbde939cb59ef5bc85
Kevin Grayson <k.grayson.ms@gmail.com>
2018-10-05 14:50:57 -0700

    Initial commit

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+export default class App extends PureComponent {
+  
+
+  render() {
+    return (
+      <div>
+        <h1>Hello react!</h1>
+      </div>
+    );
+  }
+}
